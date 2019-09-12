c1a061cea9cf20109c26d922ed85ef1eb00b5232
vparashar <vijay.parashar@zalando.de>
2019-09-12 11:06:41 +0200

    Step 9: Micro service test with multiple api calls and nakadi events

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,12 +1,14 @@
 function() {
+    var NakadiEventListener = Java.type('parashar.vijay.karate.demo.nakadi.NakadiEventListener');
+    var nakadi = NakadiEventListener.instance();
+
     var config = {
         httpBin: 'https://httpbin.org',
-        token_file: '/Users/vparashar/Documents/workspace/credentials/ztoken.txt',
-        workflow_network: 'https://workflow-repository-live.logistics.zalan.do'
+        token_file: '/Users/vparashar/Documents/workspace/credentials/token.txt',
+        workflow_network: 'https://workflow-repository-dev.logistics.zalan.do',
+        process_instance_generator: 'https://process-instance-generator-dev.logistics-test.zalan.do',
+        movementBaseURL:'https://movement-service-dev.logistics-test.zalan.do'
 
     };
     return config;
-
-
-
 }
